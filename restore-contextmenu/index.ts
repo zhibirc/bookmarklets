4a0df5e481f33f665c632c0d6cdaf15f476fa02f
Yaroslav <y.surilov@infomir.com>
2021-02-02 11:36:44 +0200

    restore-contextmenu: explain code

--- restore-contextmenu/index.ts
+++ restore-contextmenu/index.ts
@@ -1,7 +1,8 @@
 /**
- * Bypass restrictions on the display of the context menu with mouse right-click, Shift+F10, Context Menu key.
+ * Bypass restrictions on the display of the context menu with mouse right-click, Shift+F10, ContextMenu key.
  *
  * Generally, it's a bad UX practice usually used to prohibit saving images, viewing resources, and some additional actions with the page.
+ * But Shift+ContextMenu works despite of disabling :).
  */
 
 
@@ -10,6 +11,10 @@
         return true;
     };
 
+    /*
+    Code from the above may have no effect if page use "addEventListener" way. But because of listener is an anonymous function,
+    "removeEventListener" has no chance. The approach below captures "contextmenu" event, prevents it from propagate, hence prevents its listener to fire.
+    */
     window.addEventListener('contextmenu', (event : Event) : void => {
         event.stopPropagation();
     }, true);
